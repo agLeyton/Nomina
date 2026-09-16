fe980cbbcdc925a88509d2a4b9596f87d53c4a6e
Ashley Leyton <ashleyleyton15@gmail.com>
2026-09-16 15:10:45 -0500

    Creacion de bean prueba

---
+++ src/main/java/com/empresa/controller/pruebaBean.java
@@ -0,0 +1,21 @@
+package com.empresa.controller;
+
+import java.io.Serializable;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+@Named("bean")
+@SessionScoped
+public class pruebaBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5300530508376905314L;
+
+	
+	public String mensaje() {
+		return "hola";
+	}
+}
